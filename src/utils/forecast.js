fcf9edb0c6c54c104258be9cc88f48fdd55b4583
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-10-05 00:56:30 +0700

    .

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=864bd6e59cc6bd5c1fbca1c1a8ba4e02&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            console.log(body)
+            callback(undefined,
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degress out." + "\n" + "cloud amount is " + body.current.cloudcover + "\n" + "\n" + ", wind speed is " + body.current.wind_speed + " mph and wind degree is " + body.current.wind_degree + ". it's " + body.current.wind_dir + " direction." + "\n" + "humidity is " + body.current.humidity + " %." + "\n" + "\n" +
+                "localtime is " + body.location.localtime + "\n" + ". timezone id is " + body.location.timezone_id + ". the daylight is " + body.current.is_day
+            )
+        }
+    })
+}
+
+// "localtime is " + body.location.localtime + " right now. \n" + baru
+
+module.exports = forecast
